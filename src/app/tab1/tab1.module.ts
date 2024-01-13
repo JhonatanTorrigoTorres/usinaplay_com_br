6be927bfbd2f49a4a56821cc06379f414e1943da
Jhonatan Torrigo Torres <71985766+JhonatanTorrigoTorres@users.noreply.github.com>
2024-01-12 21:13:45 -0300

    feat: ajustando layout conforme modelo

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { TreinosCarrosselComponent } from '../components/treinos-carrossel/treinos-carrossel.component';
-import { AppModule } from '../app.module';
+import { ComponentsModule } from '../components.module';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -12,7 +11,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    AppModule,
+    ComponentsModule,
     Tab1PageRoutingModule
   ],
   declarations: [Tab1Page]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public usuario = {
+    nome: 'Jhonatan Torres',
+    imagem: 'imagem_usuario.png',
+    nivel: 'Roxo'
+  };
 
-  constructor() {}
+  public dados = {
+    treino: 'PERSONAL ONLINE',
+    treinos: [
+      {
+        id: 1,
+        imagem: 'imagem_yoga.png',
+        nome: 'YOGA EXPRESS'
+      },
+      {
+        id: 2,
+        imagem: 'imagem_supino.png',
+        nome: 'SUPINO RETO'
+      }
+    ],
+    programa: 'PROGRAMAS',
+    programas: [
+      {
+        id: 1,
+        imagem: 'imagem_treino.png',
+        nome: 'LEVANTAMENTO DE PESO',
+        concluido: false
+      },
+      {
+        id: 2,
+        imagem: 'imagem_supino.png',
+        nome: 'SUPINO RETO',
+        concluido: true
+      },
+      {
+        id: 3,
+        imagem: 'imagem_yoga.png',
+        nome: 'YOGA EXPRESS',
+        concluido: false
+      },
+    ],
+    conteudo: 'CONTEÚDOS'
+  };
+  constructor(
+    private navCtrl: NavController,
+  ) { }
 
+
+  navigateTo(tipo: string) {
+
+    switch (tipo) {
+      case 'Home':
+        // this.navCtrl.navigateForward('/home');
+        console.log('Foi para', tipo)
+        break;
+
+      case 'Treinos':
+        // this.navCtrl.navigateForward('/treinos');
+        console.log('Foi para', tipo)
+        break;
+
+      case 'Ajuda':
+        // this.navCtrl.navigateForward('/ajuda');
+        console.log('Foi para', tipo)
+        break;
+
+      case 'Sair':
+        // this.navCtrl.navigateForward('/sair');
+        console.log('Foi para', tipo)
+        break;
+    }
+  }
 }
