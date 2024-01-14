043663e221d3bbab4c381480643838ed16bc9d4b
Jhonatan Torrigo Torres <71985766+JhonatanTorrigoTorres@users.noreply.github.com>
2024-01-14 12:23:09 -0300

    feat: configurando banco de dados firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"usina-play-9e288","appId":"1:571213521215:web:318a16986d69bb2a66738a","storageBucket":"usina-play-9e288.appspot.com","apiKey":"AIzaSyB2WjqsrHkY6UEGY5hMrRcHzMji9Dtc57o","authDomain":"usina-play-9e288.firebaseapp.com","messagingSenderId":"571213521215","measurementId":"G-8R8VJS0K2Z"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   exports: [],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
