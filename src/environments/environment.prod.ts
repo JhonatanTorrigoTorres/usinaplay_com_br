9047b387f4c700f473a78a1c452a07de0d37c515
Jhonatan Torrigo Torres <71985766+JhonatanTorrigoTorres@users.noreply.github.com>
2024-01-14 12:48:01 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: "AIzaSyB2WjqsrHkY6UEGY5hMrRcHzMji9Dtc57o",
+    authDomain: "usina-play-9e288.firebaseapp.com",
+    projectId: "usina-play-9e288",
+    storageBucket: "usina-play-9e288.appspot.com",
+    messagingSenderId: "571213521215",
+    appId: "1:571213521215:web:318a16986d69bb2a66738a",
+    measurementId: "G-8R8VJS0K2Z"
+  },
+  
+  production: false,
+  keyLocalStorage: 'usinaplay',
 };
